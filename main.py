29c034367659391709c2843f1681cc51ef349dd3
Krixsick <linusgao123@gmail.com>
2025-12-26 18:10:35 -0500

    Minor tweaks and removed hardcoded channel id

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("DISCORD_DEV_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
